439a5bd793a9ae11f80f75e585a5a8cf5ecd0853
Shifatullah Rizwan <shifatullah@gmail.com>
2017-09-10 14:18:33 +1000

    updated angular cli project to include routing and products api call

--- Backend/Angular/CLI/ShoeStore.Backend/src/app/app.module.ts
+++ Backend/Angular/CLI/ShoeStore.Backend/src/app/app.module.ts
@@ -2,23 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { AlertModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { UserItemComponent } from './user-item/user-item.component';
 import { UserListComponent } from './user-list/user-list.component';
-import { AlertModule } from 'ngx-bootstrap';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'products', component: ProductsComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent,    
-    LoginComponent, UserItemComponent, UserListComponent
+    AppComponent, LoginComponent, UserItemComponent, UserListComponent, ProductsComponent, HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AlertModule.forRoot()
+    AlertModule.forRoot(),
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Backend/Angular/CLI/ShoeStore.Backend/src/app/products/products.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+@Component({
+    selector: 'app-products',
+    templateUrl: './products.component.html',
+    styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+    data: JSON;
+    loading: boolean;
+    _http: Http;
+
+    constructor(private http: Http) {
+        this._http = http;
+    }
+
+    fetchProducts(): void {
+        this.loading = true;        
+        this._http.request('http://shoestoreproducts-dotnet-webapi.azurewebsites.net/api/products')
+            .subscribe((res: Response) => {
+                this.loading = false;                
+                this.data = res.json();                
+            });
+    }
+
+    ngOnInit() {
+
+    }
+
+}
