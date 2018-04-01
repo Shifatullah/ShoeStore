ef87da0695d19031cb3fc6de95cf3dcf611e7a82
Shifatullah Rizwan <5946849+Shifatullah@users.noreply.github.com>
2018-04-01 14:18:00 +1000

    Created second implementation of backend using Spring MVC with Spring Boot

--- Backend/Java/SpringMVC/One/ShoeStore.Backend/src/main/java/com/shoestore/backend/HomeController.java
+++ Backend/Java/SpringMVC/One/ShoeStore.Backend/src/main/java/com/shoestore/backend/HomeController.java
@@ -24,62 +24,62 @@ import com.shoestore.model.Product;
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+
+		model.addAttribute("serverTime", formattedDate);
+
 		return "home";
 	}
-	
+
 	@Autowired
 	ProductDao dao;
-	
+
 	@RequestMapping("/addProducts")
 	public ModelAndView showform() {
 		return new ModelAndView("addProducts", "command", new Product());
 	}
-	
-	@RequestMapping(value="/addProducts", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/addProducts", method = RequestMethod.POST)
 	public ModelAndView save(@ModelAttribute(" ") Product product) {
 		dao.addProduct(product);
 		return new ModelAndView("redirect:/viewProducts");
 	}
-	
+
 	@RequestMapping("/viewProducts")
 	public ModelAndView viewProducts() {
 		List<Product> list = dao.getAllProducts();
 		return new ModelAndView("viewProducts", "list", list);
 	}
-	
+
 	@RequestMapping("/editProducts/{id}")
 	public ModelAndView edit(@PathVariable int id) {
-		Product product = dao.getProductById(id);		
+		Product product = dao.getProductById(id);
 		return new ModelAndView("editProducts", "command", product);
 	}
-	
-	@RequestMapping(value="/editAndSave",method=RequestMethod.POST)
+
+	@RequestMapping(value = "/editAndSave", method = RequestMethod.POST)
 	public ModelAndView editsave(@ModelAttribute("product") Product product) {
 		dao.updateProduct(product);
 		return new ModelAndView("redirect:/viewProducts");
-	}	
-	
+	}
+
 	@RequestMapping("/deleteProducts/{id}")
 	public ModelAndView delete(@PathVariable int id) {
 		dao.deleteProduct(id);
 		return new ModelAndView("redirect:/viewProducts");
 	}
-	
+
 }
