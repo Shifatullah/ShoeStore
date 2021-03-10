ef2f22fe349c5a5b8159b7aad8237cb80a43e9a6
Shifatullah Rizwan <5946849+Shifatullah@users.noreply.github.com>
2021-03-10 23:49:39 +1100

    test pipeline vars

--- Azure/PowerShell/ProvisionAzureResources.ps1
+++ Azure/PowerShell/ProvisionAzureResources.ps1
@@ -7,7 +7,7 @@ Write-Host "Connect to Azure using Service Principal"
 #[string] $tenant = $env:AzureTenant
 #[string] $clientId = $env:ShoeStoreId
 [string] $clientPassword = 'it is $(one)'
-Write-Host clientPassword
+Write-Host $clientPassword
 [securestring] $clientPasswordSecure = ConvertTo-SecureString -String $clientPassword -AsPlainText -Force
 #$pscredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $clientId, $clientPasswordSecure
 #Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant $tenant | Out-Null
