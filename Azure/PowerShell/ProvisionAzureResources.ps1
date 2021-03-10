1254bb40644a5680c788ca150c162faba3017647
Shifatullah Rizwan <5946849+Shifatullah@users.noreply.github.com>
2021-03-10 22:28:55 +1100

    az-pipeline fix var names

--- Azure/PowerShell/ProvisionAzureResources.ps1
+++ Azure/PowerShell/ProvisionAzureResources.ps1
@@ -1,12 +1,9 @@
 Write-Host "Provision ShoeStore resources on Azure"
 
 #Connect to Azure using Service Principal
-[string] $tenant = $env:AZURETENANT
-[string] $clientId = $env:SHOESTOREID
-[string] $clientPassword = $env:SHOESTORESECRET
+[string] $tenant = $env:AzureTenant
+[string] $clientId = $env:ShoeStoreId
+[string] $clientPassword = $env:ShoeStoreSecret
 [securestring] $clientPasswordSecure = ConvertTo-SecureString -String $clientPassword -AsPlainText -Force
 $pscredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $clientId, $clientPasswordSecure
 Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant $tenant | Out-Null
-
-#Display Tenant Id to verify it has successfully connected using Azure Service Principal
-(Get-AzContext).Tenant.Id
