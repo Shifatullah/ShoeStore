8c05fadac35fe2664be113894d98eafaae945938
Shifatullah Rizwan <5946849+Shifatullah@users.noreply.github.com>
2021-03-11 00:31:01 +1100

    test vars

--- Azure/PowerShell/ProvisionAzureResources.ps1
+++ Azure/PowerShell/ProvisionAzureResources.ps1
@@ -8,39 +8,40 @@ Write-Host "Connect to Azure using Service Principal"
 #[string] $clientId = $env:ShoeStoreId
 #[string] $clientPassword = 'it is $(one)'
 
-Write-Host '$(variables.test)' 
-Write-Host '$(test)'
-Write-Host '${{ variables.test }}'
-Write-Host '${{ test }}'
+Write-Host '$(variables.azure-tenant)' 
+Write-Host '$(azure-tenant)'
+Write-Host '${{ variables.azure-tenant }}'
+Write-Host '${{ azure-tenant }}'
 Write-Host $env:TEST
-Write-Host '$env:test'
-Write-Host '$(env:test)'
-Write-Host '${{ env:test }}'
+Write-Host $env:AZURETENANT
+Write-Host '$env:azure-tenant'
+Write-Host '$(env:azure-tenant)'
+Write-Host '${{ env:azure-tenant }}'
 if($env:TEST) { write-host "not null" }
 else { write-host "null value" }
 
-[securestring] $clientPasswordSecure1 = ConvertTo-SecureString -String '$(variables.test)' -AsPlainText -Force
+[securestring] $clientPasswordSecure1 = ConvertTo-SecureString -String '$(variables.azure-tenant)' -AsPlainText -Force
 write-host $clientPasswordSecure1
 
-[securestring] $clientPasswordSecure2 = ConvertTo-SecureString -String '$(test)' -AsPlainText -Force
+[securestring] $clientPasswordSecure2 = ConvertTo-SecureString -String '$(azure-tenant)' -AsPlainText -Force
 write-host $clientPasswordSecure2
 
-[securestring] $clientPasswordSecure3 = ConvertTo-SecureString -String '${{ variables.test }}' -AsPlainText -Force
+[securestring] $clientPasswordSecure3 = ConvertTo-SecureString -String '${{ variables.azure-tenant }}' -AsPlainText -Force
 write-host $clientPasswordSecure3
 
-[securestring] $clientPasswordSecure4 = ConvertTo-SecureString -String '${{ test }}' -AsPlainText -Force
+[securestring] $clientPasswordSecure4 = ConvertTo-SecureString -String '${{ azure-tenant }}' -AsPlainText -Force
 write-host $clientPasswordSecure4
 
-[securestring] $clientPasswordSecure5 = ConvertTo-SecureString -String $env:TEST -AsPlainText -Force
+[securestring] $clientPasswordSecure5 = ConvertTo-SecureString -String $env:AZURETENANT -AsPlainText -Force
 write-host $clientPasswordSecure5
 
-[securestring] $clientPasswordSecure6 = ConvertTo-SecureString -String '$env:test' -AsPlainText -Force
+[securestring] $clientPasswordSecure6 = ConvertTo-SecureString -String '$env:AZURETENANT' -AsPlainText -Force
 write-host $clientPasswordSecure6
 
-[securestring] $clientPasswordSecure7 = ConvertTo-SecureString -String '$(env:test)' -AsPlainText -Force
+[securestring] $clientPasswordSecure7 = ConvertTo-SecureString -String '$(env:AZURETENANT)' -AsPlainText -Force
 write-host $clientPasswordSecure7
 
-[securestring] $clientPasswordSecure8 = ConvertTo-SecureString -String '${{ env:test }}' -AsPlainText -Force
+[securestring] $clientPasswordSecure8 = ConvertTo-SecureString -String '${{ env:AZURETENANT }}' -AsPlainText -Force
 write-host $clientPasswordSecure8
 
 #$pscredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $clientId, $clientPasswordSecure
