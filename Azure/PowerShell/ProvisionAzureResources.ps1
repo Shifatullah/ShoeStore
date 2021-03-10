12839b2ae4ca3a4626c26918e61014f9e5244d22
Shifatullah Rizwan <5946849+Shifatullah@users.noreply.github.com>
2021-03-11 00:10:38 +1100

    test vars

--- Azure/PowerShell/ProvisionAzureResources.ps1
+++ Azure/PowerShell/ProvisionAzureResources.ps1
@@ -7,6 +7,16 @@ Write-Host "Connect to Azure using Service Principal"
 #[string] $tenant = $env:AzureTenant
 #[string] $clientId = $env:ShoeStoreId
 #[string] $clientPassword = 'it is $(one)'
+
+Write-Host '$(variables.one)' 
+Write-Host '$(one)'
+Write-Host '${{ variables.one }}'
+Write-Host '${{ one }}'
+Write-Host $env:ONE 
+Write-Host '$env:ONE'
+Write-Host '$(env:ONE)'
+Write-Host '${{ env:ONE }}'
+
 [securestring] $clientPasswordSecure1 = ConvertTo-SecureString -String '$(variables.one)' -AsPlainText -Force
 write-host $clientPasswordSecure1
 
