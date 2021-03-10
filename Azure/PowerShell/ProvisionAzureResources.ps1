c21ebd1712c3fdf98911353d9187c9ccc62998e1
Shifatullah Rizwan <5946849+Shifatullah@users.noreply.github.com>
2021-03-11 00:07:08 +1100

    test vars

--- Azure/PowerShell/ProvisionAzureResources.ps1
+++ Azure/PowerShell/ProvisionAzureResources.ps1
@@ -7,7 +7,29 @@ Write-Host "Connect to Azure using Service Principal"
 #[string] $tenant = $env:AzureTenant
 #[string] $clientId = $env:ShoeStoreId
 #[string] $clientPassword = 'it is $(one)'
-[securestring] $clientPasswordSecure = ConvertTo-SecureString -String $(variables.one) -AsPlainText -Force
-write-host $clientPasswordSecure
+[securestring] $clientPasswordSecure1 = ConvertTo-SecureString -String '$(variables.one)' -AsPlainText -Force
+write-host $clientPasswordSecure1
+
+[securestring] $clientPasswordSecure2 = ConvertTo-SecureString -String '$(one)' -AsPlainText -Force
+write-host $clientPasswordSecure2
+
+[securestring] $clientPasswordSecure3 = ConvertTo-SecureString -String '${{ variables.one }}' -AsPlainText -Force
+write-host $clientPasswordSecure3
+
+[securestring] $clientPasswordSecure4 = ConvertTo-SecureString -String '${{ one }}' -AsPlainText -Force
+write-host $clientPasswordSecure4
+
+[securestring] $clientPasswordSecure5 = ConvertTo-SecureString -String $env:ONE -AsPlainText -Force
+write-host $clientPasswordSecure5
+
+[securestring] $clientPasswordSecure6 = ConvertTo-SecureString -String '$env:ONE' -AsPlainText -Force
+write-host $clientPasswordSecure6
+
+[securestring] $clientPasswordSecure7 = ConvertTo-SecureString -String '$(env:ONE)' -AsPlainText -Force
+write-host $clientPasswordSecure7
+
+[securestring] $clientPasswordSecure8 = ConvertTo-SecureString -String '${{ env:ONE }}' -AsPlainText -Force
+write-host $clientPasswordSecure8
+
 #$pscredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $clientId, $clientPasswordSecure
 #Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant $tenant | Out-Null
