f38dc44e7257bed82e4c06470d5cfae689647931
Shifatullah Rizwan <5946849+Shifatullah@users.noreply.github.com>
2021-03-10 22:57:43 +1100

    testing pipeline vars

--- Azure/PowerShell/ProvisionAzureResources.ps1
+++ Azure/PowerShell/ProvisionAzureResources.ps1
@@ -1,6 +1,8 @@
 Write-Host "Provision ShoeStore resources on Azure"
 
-Write-Host $env:Test
+Write-Host $(test)
+Write-Host "test vars"
+Write-Host '${{ test }}'
 
 #Write-Host "Import Az module"
 #Install-Module -Name Az -AllowClobber -Scope CurrentUser -Force
