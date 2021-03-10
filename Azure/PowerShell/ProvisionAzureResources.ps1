5cb59365b679a6157950fc26f091c06e3d8dfdcd
Shifatullah Rizwan <5946849+Shifatullah@users.noreply.github.com>
2021-03-11 00:47:54 +1100

    test pipeline vars

--- Azure/PowerShell/ProvisionAzureResources.ps1
+++ Azure/PowerShell/ProvisionAzureResources.ps1
@@ -4,11 +4,6 @@ Write-Host "Import Az module"
 Install-Module -Name Az -AllowClobber -Scope CurrentUser -Force
 
 Write-Host "Connect to Azure using Service Principal"
-Write-Host $env:TEST
-Write-Host $env:AZURETENANT
-Write-Host $env:SHOESTOREID
-Write-Host $env:SHOESTORESECRET
-
 [securestring] $clientPasswordSecure = ConvertTo-SecureString -String $env:SHOESTORESECRET -AsPlainText -Force
 $pscredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $env:SHOESTOREID, $clientPasswordSecure
 Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant $env:AZURETENANT | Out-Null
