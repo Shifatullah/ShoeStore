17e62104481e0e8d92542879ed9668755eea02a3
Shifatullah Rizwan <5946849+Shifatullah@users.noreply.github.com>
2021-03-10 22:21:33 +1100

    removed validation attribute

--- Azure/PowerShell/ProvisionAzureResources.ps1
+++ Azure/PowerShell/ProvisionAzureResources.ps1
@@ -1,9 +1,9 @@
 Write-Host "Provision ShoeStore resources on Azure"
 
 #Connect to Azure using Service Principal
-[string][ValidateNotNullOrEmpty()] $tenant = $env:AZURETENANT
-[string][ValidateNotNullOrEmpty()] $clientId = $env:SHOESTOREID
-[string][ValidateNotNullOrEmpty()] $clientPassword = $env:SHOESTORESECRET
+[string] $tenant = $env:AZURETENANT
+[string] $clientId = $env:SHOESTOREID
+[string] $clientPassword = $env:SHOESTORESECRET
 [securestring] $clientPasswordSecure = ConvertTo-SecureString -String $clientPassword -AsPlainText -Force
 $pscredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $clientId, $clientPasswordSecure
 Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant $tenant | Out-Null
