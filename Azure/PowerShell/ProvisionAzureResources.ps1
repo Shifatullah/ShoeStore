7e95145671677e330d46f97435668791a323ab6f
Shifatullah Rizwan <5946849+Shifatullah@users.noreply.github.com>
2021-03-10 23:53:58 +1100

    test pipeline

--- Azure/PowerShell/ProvisionAzureResources.ps1
+++ Azure/PowerShell/ProvisionAzureResources.ps1
@@ -6,8 +6,8 @@ Write-Host "Import Az module"
 Write-Host "Connect to Azure using Service Principal"
 #[string] $tenant = $env:AzureTenant
 #[string] $clientId = $env:ShoeStoreId
-[string] $clientPassword = 'it is $(one)'
-Write-Host $clientPassword
-[securestring] $clientPasswordSecure = ConvertTo-SecureString -String $clientPassword -AsPlainText -Force
+#[string] $clientPassword = 'it is $(one)'
+[securestring] $clientPasswordSecure = ConvertTo-SecureString -String $(one) -AsPlainText -Force
+write-host $clientPasswordSecure
 #$pscredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $clientId, $clientPasswordSecure
 #Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant $tenant | Out-Null
